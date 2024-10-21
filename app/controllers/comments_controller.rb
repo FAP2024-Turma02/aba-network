f46d9b316b9eb63685acd690ee8fc6adadd4bae4
Nataly <natalycarvalho404@gmail.com>
2024-10-21 08:25:19 -0300

    Add create and update to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :comment, only: %i[show]
+  before_action :comment, only: %i[show update]
 
   def index
     @comments = policy_scope(Comment).order(created_at: :desc)
@@ -11,10 +11,38 @@ class CommentsController < ApplicationController
     authorize @comment
     render json: CommentSerializer.call(@comment)
   end
+  
+  def create
+    @comment = Comment.new(comment_params)
+    
+    authorize @comment
+
+    if @comment.save
+      render json: CommentSerializer.call(@comment), status: :created
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update 
+    authorize @comment
+
+    if @comment.update(comment_params)
+      render json: CommentSerializer.call(@comment), status: :ok
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue Pundit::NotAuthorizedError
+    render json: { error: 'Você não tem permissão para atualizar este comentário.' }, status: :forbidden
+  end
 
   private
 
   def comment
     @comment ||= Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(policy(@comment||Comment).permitted_attributes)
   end
 end
