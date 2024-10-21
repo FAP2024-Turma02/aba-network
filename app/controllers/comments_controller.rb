019dad6174b5d6dcf1ec6be2d92e56491c1c6fce
Nataly <natalycarvalho404@gmail.com>
2024-10-21 08:25:19 -0300

    Add create and update to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,36 @@ class CommentsController < ApplicationController
     authorize @comment
     render json: CommentSerializer.call(@comment)
   end
+  
+  def create
+    @comment = Comment.new(permitted_attributes(Comment))
+
+    if params[:post_id]
+      @comment.commentable = Post.find(params[:post_id])
+    elsif params[:comment_id]
+      @comment.commentable = Comment.find(params[:comment_id])
+    else
+      return render json: { error: 'O comentário deve estar associado a uma postagem ou outro comentário'}, status: :unprocessable_entity
+    end
+
+    authorize @comment
+
+    if @comment.save
+      render json: CommentSerializer.call(@comment), status: :created
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update 
+    authorize @comment
+
+    if @comment.update(permitted_attributes(Comment))
+      render json: CommentSerializer.call(@comment), status: :ok
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
   private
 
