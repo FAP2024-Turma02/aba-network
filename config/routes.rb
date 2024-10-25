6891e2125ed69246fbb59c08f803e2560621936e
Nataly Carvalho <natalycarvalho404@gmail.com>
2024-10-25 18:09:20 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:show, :index]
+  resources :posts, only: [:index, :show]
   resources :companies, only: [:index, :show]
   mount_devise_token_auth_for 'User', at: 'auth'
 
